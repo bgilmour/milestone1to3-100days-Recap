f643e377cc5f3e8ebf6ca6b4ce9dc8a71dcde5fb
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-06-27 14:53:43 +0100

    Initial Commit

---
+++ Milestone1to3/Milestone1to3App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone1to3App.swift
+//  Milestone1to3
+//
+//  Created by Bruce Gilmour on 2021-06-27.
+//
+
+import SwiftUI
+
+@main
+struct Milestone1to3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
